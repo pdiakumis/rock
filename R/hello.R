5dac3d0191cb978386606df06722828eef321562
pdiakumis <pdiakumis@unimelb.edu.au>
2018-03-05 10:32:13 +1100

    cleaning up a bit - part1

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,14 @@
+#' Say Hello to My Little Friend(s)
+#'
+#' Says hello to all elements in `x`
+#'
+#' @param x An atomic vector
+#' @return A character vector of length equal
+#'   to `x`, saying 'Hello' to each of its elements.
+#' @examples
+#' hello('UMCCR')
+#' hello(c('Mollie', 'Walter', 'Dexter'))
+#' @export
 hello <- function(x) {
   stopifnot(is.atomic(x))
   return(glue::glue("Hello, {x}!"))
--- R/rr-package.R
+++ R/rr-package.R
@@ -1,4 +1,19 @@
 #' rr: Rocking R at UMCCR
 #'
-#' @keywords internal
-"_PACKAGE"
+#' The `rr` package provides functions for several bioinformatic
+#' analyses:
+#'
+#' * Foo
+#' * Bar
+#' * Baz
+#'
+#' @section Foo functions:
+#' The foo functions do this, this and that.
+#'
+#' @docType package
+#' @name rr-package
+NULL
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Rrrrrrr...!")
+}
