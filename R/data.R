2f8296aa9ee2cd7e952e60f4950cd22f28f0f8db
pdiakumis <pdiakumis@unimelb.edu.au>
2018-03-05 11:14:53 +1100

    :smirk_cat: dummy data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Names of awesome cats
+#'
+#' A vector containing names of cats.
+#' Some are cereal killers.
+#'
+#' @format Atomic character vector with 3 elements
+"cat_names"
--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -9,3 +9,7 @@ test_that("non-atomics are not greeted", {
   expect_error(hello(mtcars))
   expect_error(hello(list(1, 2, 3)))
 })
+
+test_that("can run on internal data", {
+  expect_equal(length(hello(cat_names)), 3)
+})
