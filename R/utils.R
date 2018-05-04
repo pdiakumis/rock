04495c89d306de619323114fe346f6918604b244
pdiakumis <pdiakumis@unimelb.edu.au>
2018-05-04 16:22:18 +1000

    adding stamp [skip ci]

---
+++ R/utils.R
@@ -0,0 +1,8 @@
+#' Print current timestamp
+#'
+#' See https://stackoverflow.com/a/10740502/2169986
+#'
+#' @export
+stamp <- function() {
+  cat("[", format(Sys.time(), usetz = TRUE), "]\n", sep = "")
+}
