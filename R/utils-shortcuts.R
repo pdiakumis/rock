65de891208d9dce6ffbe6b3b52271a3ca3e9141c
pdiakumis <pdiakumis@unimelb.edu.au>
2018-05-26 01:49:42 +1000

    renaming files [skip ci]

--- R/utils-shortcuts.R
+++ R/utils-shortcuts.R
@@ -1,3 +1,15 @@
+#' Pipe operator
+#'
+#' See \code{dplyr::\link[dplyr]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
 #' Tidy eval helpers
 #'
 #' These functions provide tidy eval-compatible ways to capture
