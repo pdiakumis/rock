2c3497d314005c427e76567ec279239018ce44bc
pdiakumis <pdiakumis@unimelb.edu.au>
2018-05-28 15:54:53 +1000

    allowing for at least one of cnv or sv input

--- R/plot_circos.R
+++ R/plot_circos.R
@@ -19,9 +19,10 @@
 #' dev.off()
 #' }
 #' @export
-plot_circos <- function(sv, cnv) {
-
-  stopifnot(class(sv) == "sv", class(cnv) == "cnv")
+plot_circos <- function(sv = NULL, cnv = NULL) {
+  # at least one is required
+  stopifnot((!is.null(sv) && class(sv) == "sv") ||
+              (!is.null(cnv) && class(cnv) == "cnv"))
 
   # Prepare angles + colors
   chr_colors <- grDevices::rainbow(circos_data$seg_num, alpha = 0.5) %>%
@@ -29,32 +30,38 @@ plot_circos <- function(sv, cnv) {
 
   #---- SV data ----#
   # var1, var2 just dummy vars for OmicCircos
-  sv <- sv$sv %>%
-    dplyr::mutate(var1 = "var1", var2 = "var2") %>%
-    dplyr::select(.data$chrom1, .data$pos1, .data$var1,
-                  .data$chrom2, .data$pos2, .data$var2,
-                  .data$svtype) %>%
-    dplyr::mutate(col = dplyr::case_when(
-      .data$svtype == "DEL" ~ "red",
-      .data$svtype == "DUP" ~ "green",
-      .data$svtype == "INS" ~ "purple",
-      .data$svtype == "INV" ~ "orange",
-      TRUE                  ~ chr_colors[.data$chrom1]))
+  if (!is.null(sv)) {
 
-  # OmicCircos doesn't like tibbles...
-  svs_bnd <- sv %>% dplyr::filter(.data$svtype == "BND") %>% as.data.frame()
-  svs_other <- sv %>% dplyr::filter(.data$svtype != "BND") %>% as.data.frame()
+    sv <- sv$sv %>%
+      dplyr::mutate(var1 = "var1", var2 = "var2") %>%
+      dplyr::select(.data$chrom1, .data$pos1, .data$var1,
+                    .data$chrom2, .data$pos2, .data$var2,
+                    .data$svtype) %>%
+      dplyr::mutate(col = dplyr::case_when(
+        .data$svtype == "DEL" ~ "red",
+        .data$svtype == "DUP" ~ "green",
+        .data$svtype == "INS" ~ "purple",
+        .data$svtype == "INV" ~ "orange",
+        TRUE                  ~ chr_colors[.data$chrom1]))
+
+    # OmicCircos doesn't like tibbles...
+    svs_bnd <- sv %>% dplyr::filter(.data$svtype == "BND") %>% as.data.frame()
+    svs_other <- sv %>% dplyr::filter(.data$svtype != "BND") %>% as.data.frame()
+  }
 
   #---- CNV data ----#
-  cnv <- cnv$cnv %>%
-    dplyr::mutate(
-      col = dplyr::case_when(
-        tot_cn == 0 ~ "red",
-        tot_cn == 1 ~ "red",
-        tot_cn == 2 ~ "grey50",
-        tot_cn >= 3 ~ "green",
-        TRUE    ~ "black")) %>%
-    as.data.frame()
+  if (!is.null(cnv)) {
+
+    cnv <- cnv$cnv %>%
+      dplyr::mutate(
+        col = dplyr::case_when(
+          tot_cn == 0 ~ "red",
+          tot_cn == 1 ~ "red",
+          tot_cn == 2 ~ "grey50",
+          tot_cn >= 3 ~ "green",
+          TRUE    ~ "black")) %>%
+      as.data.frame()
+  }
 
   #---- Circos Plot ----#
   # turn off warnings because in their code they take max/min of character matrix...
@@ -64,20 +71,24 @@ plot_circos <- function(sv, cnv) {
 
   OmicCircos::circos(R = 400, cir = circos_data$db, type = "chr",
                      col = chr_colors, print.chr.lab = TRUE, W = 4)
-  OmicCircos::circos(R = 260, cir = circos_data$db, type = "arc",
-                     W = 120, mapping = cnv, col.v = 4, B = TRUE, lwd = 5,
-                     col = cnv$col, scale = FALSE)
-
-  # some cases where no BNDs or other SVs PASS.
-  if (nrow(svs_bnd) > 0) {
-    OmicCircos::circos(R = 260, cir = circos_data$db, type = "link",
-                       W = 40, mapping = svs_bnd, lwd = 2,
-                       col = svs_bnd$col)
+  if (!is.null(cnv)) {
+    OmicCircos::circos(R = 260, cir = circos_data$db, type = "arc",
+                       W = 120, mapping = cnv, col.v = 4, B = TRUE, lwd = 5,
+                       col = cnv$col, scale = FALSE)
   }
-  if (nrow(svs_other) > 0) {
-    OmicCircos::circos(R = 260, cir = circos_data$db, type = "link2",
-                       W = 20, mapping = svs_other, lwd = 1,
-                       col = svs_other$col)
+
+  if (!is.null(sv)) {
+    # some cases where no BNDs or other SVs PASS.
+    if (nrow(svs_bnd) > 0) {
+      OmicCircos::circos(R = 260, cir = circos_data$db, type = "link",
+                         W = 40, mapping = svs_bnd, lwd = 2,
+                         col = svs_bnd$col)
+    }
+    if (nrow(svs_other) > 0) {
+      OmicCircos::circos(R = 260, cir = circos_data$db, type = "link2",
+                         W = 20, mapping = svs_other, lwd = 1,
+                         col = svs_other$col)
+    }
   }
 
 }
@@ -86,5 +97,5 @@ plot_circos <- function(sv, cnv) {
 #sv <- rock::prep_manta_vcf("/Users/pdiakumis/Desktop/projects/umccr/tothill_projects/data/a5/vcf/structural/E019-manta.vcf.gz")
 #cnv <- rock::prep_facets_seg("/Users/pdiakumis/Desktop/projects/umccr/tothill_projects/data/a5/facets/batch1/E019/E019_cval_150_fit.rds")
 #pdf("~/Desktop/tmp/circos2.pdf", width = 7, height = 7)
-#rock::plot_circos(sv = sv, cnv = cnv)
+#rock::plot_circos(sv = NULL, cnv = cnv)
 #dev.off()
