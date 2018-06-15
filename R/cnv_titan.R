df88d3ddac82e0817c23fbb386395ea49df19246
pdiakumis <pdiakumis@unimelb.edu.au>
2018-06-16 02:24:24 +1000

    read titan seg

--- R/cnv_titan.R
+++ R/cnv_titan.R
@@ -1,19 +1,53 @@
-#-- Functions for working with TitanCNA --#
+#' Read TitanCNA CNV Segments
+#'
+#' Reads the `sample-titan.segs.tsv` file output by TitanCNA and exports the
+#' CNV segment coordinates.
+#'
+#' @param titan Path to TitanCNA `sample-titan.segs.tsv` file.
+#' @return A dataframe (`tibble`) with the following columns:
+#'   * chrom: chromosome
+#'   * start: start coordinate
+#'   * end: end coordinate
+#'   * tot_cn: total copy number estimate
+#'
+#' @examples
+#' cn <- system.file("extdata", "HCC2218_titan.segs.tsv", package = "pebbles")
+#' prep_titan_seg(cn)
+#'
+#' @export
+prep_titan_seg <- function(titan) {
 
+  cnv <- read_titan_seg(titan) %>%
+    dplyr::select(.data$chrom, .data$start, .data$end, .data$tot_cn) %>%
+    dplyr::filter(.data$chrom != "MT")
 
-#.titan_read_segs <- function(fname) {
-#  readr::read_tsv(fname, col_names = TRUE, col_types = "cciiidcdiciiiid")
-#}
-#
-## See bcbio/structural/titancna.py
-#.titan_get_sv_type <- function(x) {
-#  titan_calls <- c(HOMD  = "DEL", DLOH  = "DEL", NLOH  = "LOH", HET   = "CNV",
-#                   ALOH  = "DUP", GAIN  = "DUP", ASCNA = "DUP", BCNA  = "DUP",
-#                   UBCNA = "DUP", OUT   = "CNV")
-#  stopifnot(all(x %in% names(titan_calls)))
-#  unname(titan_calls[x])
-#}
-#
-# segs_fname <- "sample1_cluster01.segs.txt"
-# segs <- .titan_read_segs(segs_fname)
-# .titan_get_sv_type(segs$TITAN_call) %>% table()
+  structure(list(cnv = cnv), class = "cnv")
+}
+
+# read titan seg file
+read_titan_seg <- function(titan) {
+
+  stopifnot(file.exists(titan))
+
+  cnv <- readr::read_tsv(titan, col_types = "cciiidcdiciiiid") %>%
+    dplyr::select(.data$Chromosome, .data$Start_Position.bp., .data$End_Position.bp., .data$Copy_Number,
+                  .data$Median_logR, .data$TITAN_call) %>%
+    dplyr::mutate(sv_type = .titan_get_sv_type(.data$TITAN_call)) %>%
+    dplyr::rename(chrom = .data$Chromosome,
+                  start = .data$Start_Position.bp.,
+                  end = .data$End_Position.bp.,
+                  tot_cn = .data$Copy_Number)
+
+  return(cnv)
+
+}
+
+# translate Titan call to SV type
+.titan_get_sv_type <- function(x) {
+  titan_calls <- c(HOMD  = "DEL", DLOH  = "DEL", NLOH  = "LOH", HET   = "CNV",
+                   ALOH  = "DUP", GAIN  = "DUP", ASCNA = "DUP", BCNA  = "DUP",
+                   UBCNA = "DUP", OUT   = "CNV")
+  stopifnot(all(x %in% names(titan_calls)))
+  unname(titan_calls[x])
+}
+
