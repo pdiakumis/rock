c92c4852651e50eca87eb7aa57eca94dc89e42ac
pdiakumis <pdiakumis@unimelb.edu.au>
2018-06-22 17:31:24 +1000

    test facets chrom

--- tests/testthat/test-cnv-facets.R
+++ tests/testthat/test-cnv-facets.R
@@ -2,7 +2,13 @@ context("FACETS tsv")
 
 facets <- system.file("extdata", "HCC2218_facets_cncf.tsv", package = "pebbles")
 cn_facets <- prep_facets_seg(facets)
+tab_chrom <- table(cn_facets$cnv$chrom)
 
 test_that("object is of cnv class", {
   expect_equal(class(cn_facets), "cnv")
+})
+
+test_that("chrom 23 is X", {
+  expect_true("X" %in% names(tab_chrom))
+  expect_false("23" %in% names(tab_chrom))
 })
