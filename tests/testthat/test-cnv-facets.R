6ae86045acaeb6ce8bed12880e29a10ed7c90674
pdiakumis <pdiakumis@unimelb.edu.au>
2018-08-30 11:57:21 +1000

    test read_cnv

--- tests/testthat/test-cnv-facets.R
+++ tests/testthat/test-cnv-facets.R
@@ -2,11 +2,13 @@ context("FACETS tsv")
 
 facets <- system.file("extdata", "HCC2218_facets_cncf.tsv", package = "pebbles")
 cn_facets <- prep_facets_seg(facets)
+cn_facets2 <- read_cnv(facets)
 tab_chrom <- table(cn_facets$cnv$chrom)
 gr <- cnv2gr(cn_facets)
 
 test_that("object is of cnv class", {
   expect_equal(class(cn_facets), "cnv")
+  expect_equal(class(cn_facets2), "cnv")
 })
 
 test_that("chrom 23 is X", {
